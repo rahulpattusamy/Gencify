fa9765ac717b32285fa470f7ec2c98c39745aaf5
rahul <rahulpattusamy@gmail.com>
2025-07-07 13:51:35 +0530

    created dynamic price in the price detail page

--- gencify/src/components/PriceDetails.tsx
+++ gencify/src/components/PriceDetails.tsx
@@ -1,40 +1,42 @@
 import usecartLenght from "../hooks/usecartLenght";
-import type { Products } from "../hooks/useProducts";
 import useProductstore from "../store";
 
 const PriceDetails = () => {
-  const cart = useProductstore((s) => s.productQuery.cart);
-  const totalPrice = (cart?: Products[]) =>
-    cart?.reduce((acc, result) => acc + parseInt(result.price), 0);
-  const total = totalPrice(cart);
-  const platformfee = 7;
-  const cartlength = usecartLenght()
-  if(cart)
-  return (
-    <div className="priceCard ">
-      <p className=" text-lg  text-black border-b-1 text-center">
-        Price Details
-      </p>
-      <p className="pricedetails">
-        Price:<span>&#36;{total}</span>
-      </p>
-      <p className="pricedetails">
-        Total Item: <span>{cartlength}</span>
-        {}
-      </p>
-      <p className="pricedetails">
-        Delivery Fee: <span className="text-green-500">Free</span>
-      </p>
-      <p className="pricedetails border-b-1 border-black">
-        Platform Fee:<span>&#36;{platformfee}</span>
-      </p>
-      <p className="pricedetails  text-black">
-        Total Amount:<span>&#36;{(total ?? 0) + (platformfee ?? 0)}</span>
-      </p>
-
-      <button className="btn ml-15">Proceed to Checkout</button>
-    </div>
+  const total = useProductstore((s) =>
+    s.productQuery.cart?.reduce(
+      (acc, result) => acc + parseInt(result.price) * result.quantity,
+      0
+    )
   );
+  const platformfee = 7;
+  const cartlength = usecartLenght();
+  const cart = useProductstore(s=>s.productQuery.cart)
+  if (cart)
+    return (
+      <div className="priceCard ">
+        <p className=" text-lg  text-black border-b-1 text-center">
+          Price Details
+        </p>
+        <p className="pricedetails">
+          Price:<span>&#36;{total}</span>
+        </p>
+        <p className="pricedetails">
+          Total Item: <span>{cartlength}</span>
+          {}
+        </p>
+        <p className="pricedetails">
+          Delivery Fee: <span className="text-green-500">Free</span>
+        </p>
+        <p className="pricedetails border-b-1 border-black">
+          Platform Fee:<span>&#36;{platformfee}</span>
+        </p>
+        <p className="pricedetails  text-black">
+          Total Amount:<span>&#36;{(total ?? 0) + (platformfee ?? 0)}</span>
+        </p>
+
+        <button className="btn ml-15">Proceed to Checkout</button>
+      </div>
+    );
 };
 
 export default PriceDetails;
