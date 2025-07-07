dd89b6f1381bcb55f74ce4f2426731f86a8c057f
rahul <rahulpattusamy@gmail.com>
2025-07-07 09:28:51 +0530

    moved product from cart to wishlist

--- gencify/src/components/PriceDetails.tsx
+++ gencify/src/components/PriceDetails.tsx
@@ -6,7 +6,7 @@ const PriceDetails = () => {
   const totalPrice = (cart?: Products[]) =>
     cart?.reduce((acc, result) => acc + parseInt(result.price), 0);
   const total = totalPrice(cart);
-  const platformfee = Math.floor (Math.random()*10)+10
+  const platformfee = 7;
   if(cart)
   return (
     <div className="priceCard ">
--- gencify/src/pages/CartDetailPage.tsx
+++ gencify/src/pages/CartDetailPage.tsx
@@ -3,63 +3,73 @@ import PriceDetails from "../components/PriceDetails";
 import useProductstore from "../store";
 import { MdOutlineDelete } from "react-icons/md";
 import { MdFavoriteBorder } from "react-icons/md";
-
+import { FindProductInWishlist } from "../utils/FindProductInWishlist";
+import { FaCheckCircle } from "react-icons/fa";
 
 const CartDetailPage = () => {
   const cart = useProductstore((s) => s.productQuery.cart);
   const removeFromCart = useProductstore((s) => s.removeFromcart);
+  const wishlist = useProductstore(s=>s.productQuery.wishlist)
+  const Setwishlist = useProductstore((s) => s.setWishlist);
+  
 
   if (!cart) return <Emptycart />;
-  if(cart.length == 0) return<Emptycart/>
+  if (cart.length == 0) return <Emptycart />;
   return (
     <>
-    <p className="text-center text-xl mt-5">My Cart</p>
-    <div className="flex mt-10 gap-20 px-30">
-    <div className="flex flex-col gap-y-5">
-      {cart?.map((item) => (
-        <div className="cartcard flex" key={item.id}>
-          <div className="left">
-            <img
-              className="w-33 h-full object-cover"
-              src={item.images}
-              alt=""
-            />
-          </div>
-          <div className="right mt-1.5 ml-7">
-            <h1 className=" w-60 text">{item.title}</h1>
-            <h2 className="mt-1">
-              <span className="text">Price: &#36;</span>
-              <span className="text">{item.price}</span>
-            </h2>
-            <div className="mt-1">
-              <h3 className="h-5 text-center text flex">
-                Quantity:
-                <div className="flex w-20 justify-between ml-5">
-                  <button className="btn4">-</button> <p>1</p>
-                  <button className="btn4">+</button>
+      <p className="text-center text-xl mt-5">My Cart</p>
+      <div className="flex mt-10 gap-20 px-30">
+        <div className="flex flex-col gap-y-5">
+          {cart?.map((item) =>{ 
+            const isProductInWishlist = FindProductInWishlist(wishlist,item.id)
+            return(
+            <div className="cartcard flex" key={item.id}>
+              <div className="left">
+                <img
+                  className="w-33 h-full object-cover"
+                  src={item.images}
+                  alt=""
+                />
+              </div>
+              <div className="right mt-1.5 ml-7">
+                <h1 className=" w-60 text">{item.title}</h1>
+                <h2 className="mt-1">
+                  <span className="text">Price: &#36;</span>
+                  <span className="text">{item.price}</span>
+                </h2>
+                <div className="mt-1">
+                  <h3 className="h-5 text-center text flex">
+                    Quantity:
+                    <div className="flex w-20 justify-between ml-5">
+                      <button className="btn4">-</button> <p>1</p>
+                      <button className="btn4">+</button>
+                    </div>
+                  </h3>
                 </div>
-              </h3>
+                <div className="button flex justify-between mt-3 w-55">
+                  <button
+                    onClick={() => removeFromCart(item.id)}
+                    className="btn3 bg-red-500"
+                  >
+                    <MdOutlineDelete />
+                    Remove
+                  </button>
+                  <button
+                    onClick={() => !isProductInWishlist && Setwishlist(item)}
+                    className="btn2 p-1 bg-red-500"
+                  >
+                  
+                    {isProductInWishlist?<FaCheckCircle />  :<MdFavoriteBorder/>} 
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="button flex justify-between mt-3 w-55">
-              <button
-                onClick={() => removeFromCart(item.id)}
-                className="btn3 bg-red-500"
-              >
-                <MdOutlineDelete />
-                Remove
-              </button>
-              <button className="btn3 bg-red-500">
-                Move to <MdFavoriteBorder/>
-              </button>
-            </div>
-          </div>
+          )})}
         </div>
-      ))}
-    </div>
-    <div className="ml-10 flex">
-      <PriceDetails/>
-    </div>
-    </div>
+        <div className="ml-10 flex">
+          <PriceDetails />
+        </div>
+      </div>
     </>
   );
 };
